64e3357a75a69a40510fd13b80a6f198d53d9260
parchinski <bryantpdev@proton.me>
2024-01-02 01:19:56 -0500

    db updated buttons fixed

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -8,5 +8,12 @@ export const CreateBoard = z.object({
     })
     .min(3, {
       message: "Title is too short.",
+    })
+    .max(25, {
+      message: "Title is too long.",
     }),
+  image: z.string({
+    required_error: "Image is required",
+    invalid_type_error: "Image is required",
+  }),
 });
--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-//! Video at 17:52
-
 import { useEffect, useState } from "react";
 import { useFormStatus } from "react-dom";
 import Image from "next/image";
-import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { Check, Loader2 } from "lucide-react";
 
 import { unsplash } from "@/lib/unsplash";
 import { cn } from "@/lib/utils";
 import { defaultImages } from "@/constants/images";
+
+import { FormErrors } from "./form-errors";
 
 interface FormPickerProps {
   id: string;
@@ -19,8 +20,9 @@ interface FormPickerProps {
 export const FormPicker = ({ id, errors }: FormPickerProps) => {
   const { pending } = useFormStatus();
 
-  const [images, setImages] =
-    useState<Array<Record<string, any>>>(defaultImages);
+  const [images, setImages] = useState<Array<Record<string, any>>>(
+    defaultImages
+  );
   const [isLoading, setIsLoading] = useState(true);
   const [selectedImageId, setSelectedImageId] = useState(null);
 
@@ -65,22 +67,45 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
             key={image.id}
             className={cn(
               "cursor-pointer relative aspect-video group hover-opacity-75 transition bg-muted",
-              pending && "opacity-50 hover:opacity-50 cursor-auto",
+              pending && "opacity-50 hover:opacity-50 cursor-auto"
             )}
             onClick={() => {
               if (pending) return;
               setSelectedImageId(image.id);
             }}
           >
+            <input
+              type="radio"
+              id={id}
+              name={id}
+              className="hidden"
+              checked={selectedImageId === image.id}
+              disabled={pending}
+              value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
+              readOnly
+            />
             <Image
               src={image.urls.thumb}
               alt="Unsplash image"
               className="object-cover rounded-sm"
               fill
             />
+            {selectedImageId === image.id && (
+              <div className="absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center">
+                <Check className="h-4 w-4 text-white" />
+              </div>
+            )}
+            <Link
+              href={image.links.html}
+              target="_blank"
+              className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[6px] truncate text-white hover:underline p-1 bg-black/50"
+            >
+              {image.user.name}
+            </Link>
           </div>
         ))}
       </div>
+      <FormErrors id="image" errors={errors} />
     </div>
   );
 };
